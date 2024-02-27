0aeb45a7bd4aaa971588914df5e0006617de15a0
jasmin7111 <mathewjasmin48@gmail.com>
2024-02-27 15:51:50 +1100

    new commit

---
+++ Turnup2024/Program.cs
@@ -0,0 +1,26 @@
+﻿//open chrome/firefox browser
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch Tunup portal and navigate to login page
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+
+//Identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+
+//Identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+
+//Identify login button and click on login button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+//check if the user has logged in successfully
